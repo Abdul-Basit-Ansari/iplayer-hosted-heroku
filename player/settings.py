0cabc2a86979cccfe19aece1e634e7e52385641b
Abdul Basit <abdulbasitansari288@gmail.com>
2022-06-17 21:14:41 +0500

    Edit MEDIA_DIRS variable

--- player/settings.py
+++ player/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/staticfiles/'
 STATIC_ROOT= os.path.join(BASE_DIR , 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    # os.path.join(BASE_DIR, 'staticfiles'),
     os.path.join(BASE_DIR, 'static'),
 ]
 
