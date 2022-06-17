ef061068d3ad5b4551bb1c8148677090a8fcc457
Abdul Basit <abdulbasitansari288@gmail.com>
2022-06-17 21:12:24 +0500

    add statticfiles folder

--- player/settings.py
+++ player/settings.py
@@ -123,10 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR , 'static')
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT= os.path.join(BASE_DIR , 'staticfiles')
 
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
     os.path.join(BASE_DIR, 'static'),
 ]
 
