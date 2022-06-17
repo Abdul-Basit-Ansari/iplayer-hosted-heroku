ebbec71ab8af10edcb7dc089bdbcb9df2f3d4c53
Abdul Basit <abdulbasitansari288@gmail.com>
2022-06-18 00:38:44 +0500

    add url

--- player/settings.py
+++ player/settings.py
@@ -132,25 +132,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR , 'staticfiles')
 
-STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'staticfiles'),
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = 'iplayer-youtubeclone.herokuapp.com/static/'
+STATIC_ROOT = 'iplayer-youtubeclone.herokuapp.com/staticfiles'
+STATICFILES_DIRS = (
+                    'iplayer-youtubeclone.herokuapp.com/static',
+                    'iplayer-youtubeclone.herokuapp.com/media',
+                   )
+PROJECT_PATH = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR , 'media')
-
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
+MEDIA_URL = 'iplayer-youtubeclone.herokuapp.com/media/'
+MEDIA_ROOT = 'iplayer-youtubeclone.herokuapp.com/media' 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MESSAGE_TAGS ={
--- player/urls.py
+++ player/urls.py
@@ -24,11 +24,8 @@ from player.settings import DEBUG
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("vplayer.urls")),
-    r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT},
-    r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT},
-    re_path(r'"media/(?P<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT}),
-    re_path(r'"static/(?P<path>.*)$',serve,{'document_root': settings.STATIC_ROOT})
+    re_path(r'"media/(?P<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT})
 ]
 # if DEBUG == False:
-# urlpatterns +=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns +=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 # urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
